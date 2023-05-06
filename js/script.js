88498088f443770e9c109779854d910830fcbf1f
Deniska122 <126402922+Deniska122@users.noreply.github.com>
2023-05-06 19:05:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let time = 5;
+
+if (time === 12 || time < 12) {
+    console.log ("Good morning!")
+} else if (time <= 16) {
+    console.log ("Good afternoon!")
+} else if (time <= 22) {
+    console.log ("Good evening!")
+} else if (time > 22 && time < 14) {
+    console.log ("Good night!")
+}
